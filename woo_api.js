d23a364314a82a909ff4c8927b092e02c2a1f269
Anbu Raj <50450017+anburaj336@users.noreply.github.com>
2024-11-19 09:27:54 +0400

    Update woo_api.js

--- woo_api.js
+++ woo_api.js
@@ -1,7 +1,7 @@
 class WooApi {
   // https://bora.co.th
   // https://mstore.io
-  website = "https://mstore.io";
+  website = "https://oldnestfarms.com";
   baseUrl = this.website + "/wp-json/api/";
   headers = { "Content-Type": "application/json" };
 
